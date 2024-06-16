cfa9a1f639aebac8a7f590a5fe13e317d6c97117
Bryan1093 <bryan_loya_1995@hotmail.com>
2024-06-16 00:41:48 -0500

    Correcion Envio programa incorrecto

--- src/main/java/OneToMany/Repository/LibraryRepository.java
+++
@@ -1,9 +0,0 @@
-package OneToMany.Repository;
-
-import OneToMany.Entity.Library;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibraryRepository extends JpaRepository<Library, Long> {
-}
